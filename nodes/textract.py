4b05e36cf30c0975f25592829a1fe30503debe71
tangqy <121102723@qq.com>
2024-07-18 07:06:45 +0800

    udpate textract node

--- nodes/textract.py
+++ nodes/textract.py
@@ -13,6 +13,8 @@ import torch
 import cv2
 import folder_paths
 from .session import get_client
+import torch
+from torchvision import transforms
 
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
@@ -112,8 +114,8 @@ class ImageOCRByTextractV2:
                     },
                 }
 
-    RETURN_TYPES = ("STRING","STRING","STRING")
-    RETURN_NAMES = ("Texts","Original Image Path","Mask Image Path")
+    RETURN_TYPES = ("STRING","STRING","STRING","IMAGE")
+    RETURN_NAMES = ("Texts","Original Image Path","Mask Image Path","AnyText Mask Image")
     FUNCTION = "forward"
     CATEGORY = "aws"
     OUTPUT_NODE = True
@@ -121,6 +123,15 @@ class ImageOCRByTextractV2:
     def ocr_by_textract(self,image_input):
         ori_image_path = folder_paths.get_annotated_filepath(image_input)
         pos_img_path = os.path.join(temp_img_path)
+
+        ## 转换ori_image为tensor张量
+        pil_image = Image.open(ori_image_path)
+        transform = transforms.Compose([
+            transforms.ToTensor(),  # 将 PIL Image 转换为 tensor，并将像素值归一化到 [0, 1]
+        ])
+        image_input = transform(pil_image)
+
+        ## image input已经是标准comfyui的image张量格式
         numpy_image = (image_input[0] * 255.0).clamp(0, 255).numpy()
         image = image_input[0] * 255.0
         image = Image.fromarray(image.clamp(0, 255).numpy().round().astype(np.uint8))
@@ -136,7 +147,8 @@ class ImageOCRByTextractV2:
         # 初始化结果列表
         result = []
         # 创建一个与原始图像大小相同的黑色遮罩图像
-        mask = np.zeros_like(numpy_image)
+        #mask = np.zeros_like(numpy_image)
+        mask = np.ones_like(numpy_image) * 255
         all_text=""
 
 
@@ -157,19 +169,27 @@ class ImageOCRByTextractV2:
                 temp_mask = np.zeros_like(numpy_image)
                 x1, y1 = int(left), int(top)
                 x2, y2 = int(left + width), int(top + height)
-                # 在遮罩图像上绘制白色矩形框
-                cv2.rectangle(temp_mask, (x1, y1), (x2, y2), (0, 0, 0), -1)
-                mask = cv2.bitwise_or(mask, temp_mask)
+                # 在遮罩图像上绘制黑色矩形框
+                #cv2.rectangle(temp_mask, (x1, y1), (x2, y2), (0, 0, 0), -1)
+                #mask = cv2.bitwise_or(mask, temp_mask)
+
+                cv2.rectangle(mask, (left, top), (left + width, top + height), (0, 0, 0), -1)
 
         # 将遮罩图像和原始图像进行位运算,生成遮罩后的图像
-        masked_img = cv2.bitwise_and(numpy_image, mask)
+        masked_img = np.where(mask == 0, 0, numpy_image)
+        #masked_img = cv2.bitwise_and(numpy_image, mask)
         masked_img = torch.from_numpy(np.array(masked_img).astype(np.float32) / 255.0).unsqueeze(0)
-        masked_img.save(temp_img_path)
-        all_text="|".join(result)
+        #masked_img.save(temp_img_path)
+        # 将 PyTorch 张量转换为 PIL 图像
+        to_pil = transforms.ToPILImage()
+        pil_image = to_pil(masked_img.squeeze(0))
+        # 保存 PIL 图像
+        pil_image.save(temp_img_path)
 
+        all_text="|".join(result)
         print("result",result)
 
-        return all_text ,ori_image_path,temp_img_path
+        return all_text ,ori_image_path,temp_img_path, masked_img
 
 
 
