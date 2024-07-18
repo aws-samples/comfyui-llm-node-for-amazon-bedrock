7e6d4b975a0ccaaa8510da4b570f19a496a566f1
lazydragon <lazy.dragon@hotmail.com>
2024-07-18 23:11:55 +0800

    Add prompt template with 2 inputs

--- nodes/prompts.py
+++ nodes/prompts.py
@@ -20,6 +20,26 @@ class PromptTemplate:
         output = prompt_template.replace("{prompt}", prompt).replace("[prompt]", prompt)
         return (output,)
 
+class PromptTemplateTwoInput:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "prompt": ("STRING", {"multiline": True}),
+                "prompt2": ("STRING", {"multiline": True}),
+                "prompt_template": ("STRING", {"multiline": True}),
+            }
+        }
+
+    RETURN_TYPES = ("STRING",)
+    FUNCTION = "forward"
+
+    CATEGORY = "utils"
+
+    def forward(self, prompt, prompt2, prompt_template):
+        output = prompt_template.replace("{prompt}", prompt).replace("[prompt]", prompt)
+        output = output.replace("{prompt2}", prompt2).replace("[prompt2]", prompt2)
+        return (output,)
 
 class PromptRegexRemove:
     @classmethod
@@ -43,5 +63,6 @@ class PromptRegexRemove:
 
 NODE_CLASS_MAPPINGS = {
     "Prompt Template": PromptTemplate,
+    "Prompt Template with Two Inputs": PromptTemplateTwoInput,
     "Prompt Regex Remove": PromptRegexRemove,
 }
