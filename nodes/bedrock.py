3407a18ce108f78064a691a4a8fa7395892feda3
FuChen <fuchen@foxmail.com>
2024-07-19 14:12:10 +0800

    update readme, add bedrock sonnet 3.5 model id

--- nodes/bedrock.py
+++ nodes/bedrock.py
@@ -171,6 +171,7 @@ class BedrockClaude:
                 "prompt": ("STRING", {"multiline": True}),
                 "model_id": (
                     [
+                        "anthropic.claude-3-5-sonnet-20240620-v1:0",
                         "anthropic.claude-3-haiku-20240307-v1:0",
                         "anthropic.claude-3-sonnet-20240229-v1:0",
                         "anthropic.claude-3-opus-20240229-v1:0",
